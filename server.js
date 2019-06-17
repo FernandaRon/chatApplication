af845dafe81a415e53a6c870b52b83b91b0dbb61
FernandaRon <rondon.fernanda@gmail.com>
2019-06-17 16:13:23 -0500

    config users

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const io = require('socket.io')(3000)
 
+const users = {}
+
 io.on('connection', socket => {
-    console.log('new user')
-    socket.emit('chat-message','Hello world')
+    socket.on('new-user', name => {
+        users[socket.id] = name 
+    })
     socket.on('send-chat-message', message => {
         socket.broadcast.emit('chat-message', message)
-    })
+    } )
 }) 
