bdfff4efd5576f83b9bbe33a422d156f031ee5ab
FernandaRon <rondon.fernanda@gmail.com>
2019-06-17 16:39:14 -0500

    conexion de otros usuarios, visible

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ const messageInput = document.getElementById('message-input');
 
 const name = prompt('What is your name?')
 appendMessage('You joined')
+socket.emit('new-user', name)
 
 socket.on('chat-message', data => {
    appendMessage(data)
 })
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+    })
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault();
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const users = {}
 io.on('connection', socket => {
     socket.on('new-user', name => {
         users[socket.id] = name 
+        socket.broadcast.emit('user-connected', name)
     })
     socket.on('send-chat-message', message => {
         socket.broadcast.emit('chat-message', message)
