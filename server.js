48072ab011f26fc9899547283ea614b2d8a9e1f8
FernandaRon <rondon.fernanda@gmail.com>
2019-06-17 16:02:33 -0500

    server, script and html

---
+++ server.js
@@ -0,0 +1,9 @@
+const io = require('socket.io')(3000)
+
+io.on('connection', socket => {
+    console.log('new user')
+    socket.emit('chat-message','Hello world')
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', message)
+    })
+}) 
